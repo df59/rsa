977e649d47099413daa52a45f1ef479c10bd8a9e
Dustin Franklin <df59@zips.uakron.edu>
2023-02-15 10:41:05 -0500

    Add working code

---
+++ key_generation/rsa435.cc
@@ -0,0 +1,123 @@
+// You need to complete this program for a part of your first project.
+
+// Standard libraries
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <stdlib.h>
+#include <string>
+
+// 'BigIntegerLibrary.hh' includes all of the library headers.
+#include "BigIntegerAlgorithms.hh"
+#include "BigIntegerLibrary.hh"
+#include "BigUnsigned.hh"
+
+int big_int_size = 200; // desired base 10 digits for large primes
+ 
+BigUnsigned get_random_bigunsigned() {
+  BigUnsigned big1 = BigUnsigned(1);
+
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> dist(0, 9);
+  big1 = dist(rng);
+
+  for (int i = 0; i < big_int_size - 1; i++) { // ~199 base 10 digit number
+    big1 = big1 * 10 + dist(rng);
+  }
+  return big1;
+}
+
+void amend_seven(BigUnsigned &number) {
+  number = number * 10;
+  number = number + 7;
+}
+
+bool fermat_test(BigUnsigned candidate_l) {
+  BigUnsigned exponent = candidate_l - 1;
+  if (candidate_l < 2) { // if this ever happens you should buy a lottery ticket
+    std::cout << "fermat test failed with candidate being less than 2\n";
+    return false;
+  }
+
+  for (int i = 1; i <= big_int_size; i++) {
+    // std::cout << "fermat test at iteration " << i << '\n';
+    BigUnsigned base = candidate_l - i;
+    if (modexp(base, exponent, candidate_l) != 1) {
+      // std::cout << "fermat test failed at iteration " << i << '\n';
+      return false;
+    }
+  }
+
+  return true;
+}
+
+BigUnsigned generate_fermat_prime() {
+  bool prime = false;
+  BigUnsigned random_candidate;
+
+  while (!prime) {
+    // std::cout << "generating new candidate\n";
+    random_candidate = get_random_bigunsigned();
+    amend_seven(random_candidate);
+    // std::cout << random_candidate << '\n';
+
+    if (fermat_test(random_candidate)) {
+      prime = true;
+      // std::cout << "fermat test passed with " << 1 << " iterations\n";
+    } else {
+      // std::cout << "fermat test failed with " << 1 << " iterations\n";
+    }
+  }
+
+  return random_candidate;
+}
+
+void save_p_q_file(BigUnsigned p, BigUnsigned q) {
+  std::ofstream p_q_file = std::ofstream{"p_q.txt"};
+  p_q_file << p << '\n' << q;
+  p_q_file.close();
+}
+
+void save_e_n_file(BigUnsigned e, BigUnsigned n) {
+  std::ofstream e_n_file = std::ofstream{"e_n.txt"};
+  e_n_file << e << '\n' << n;
+  e_n_file.close();
+}
+
+void save_d_n_file(BigUnsigned d, BigUnsigned n) {
+  std::ofstream d_n_file = std::ofstream{"d_n.txt"};
+  d_n_file << d << '\n' << n;
+  d_n_file.close();
+}
+
+int main() {
+  // p and q generation of large fermat primes
+  BigUnsigned p = generate_fermat_prime();
+  std::cout << "p has been generated\n";
+  BigUnsigned q = generate_fermat_prime();
+  std::cout << "q has been generated\n";
+  save_p_q_file(p, q);
+  std::cout << "p and q written to p_q.txt\n";
+
+  // n, e, d calculation of public and private keys
+  BigUnsigned n, e, d, totient;
+  n = p * q;
+  std::cout << "n has been calculated\n" << n << '\n';
+  e = 2316961; // public known prime number
+  std::cout << "e is " << e << '\n';
+  totient = (p - 1) * (q - 1) / gcd(p - 1, q - 1);
+  d = modinv(e, totient); // extended euclidian implementation utilized in
+                              // BigIntegerAlgorithms
+  std::cout << "d has been calculated\n" << d << '\n';
+
+
+  save_e_n_file(e, n);
+  std::cout << "e and n written to e_n.txt\n";
+  save_d_n_file(d, n);
+  std::cout << "d and n written to d_n.txt\n";
+
+
+  return 0;
+}
---
+++ sign_verify/messageDigest435.cpp
@@ -0,0 +1,128 @@
+/*
+Dustin Franklin
+df59@uakron.edu
+University of Akron Computer Science
+Algorithms Spring 2023
+
+To build this program run:
+
+make clean
+make
+
+This program is executed in the command line in one of two forms:
+
+./executable s file.xyz
+
+where s indicates the program will sign file.xyz and save the signature of the sha256 hash of file.xyz to file.xyz.signature.
+It is required to have a private key pair file named d_n.txt in the working directory.
+
+or
+
+./executable v file.xyz file.xyz.signature
+
+where v indicates the program will verify that the hash of file.xyz matches file.xyz.signature.
+It is required to have a public key pair file named e_n.txt in the working directory.
+
+
+*/
+
+#include "BigIntegerAlgorithms.hh"
+#include "BigIntegerLibrary.hh"
+#include "BigUnsigned.hh"
+#include "BigUnsignedInABase.hh"
+#include "sha256.h"
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <string>
+
+void save_signature_file(BigUnsigned signature, std::string filename) {
+  std::string sig_file_name = filename + ".signature";
+  std::ofstream signature_file = std::ofstream{sig_file_name};
+  signature_file << signature;
+  std::cout << "Signature written to " << sig_file_name << '\n';
+  signature_file.close();
+}
+
+int main(int argc, char *argv[]) {
+  // Second part of your project starts here
+  if (argc > 4 || argc < 3 || (argv[1][0] != 's' && argv[1][0] != 'v'))
+    std::cout << "wrong format! should be \"a.exe s filename\"";
+  else {
+    std::string filename = argv[2];
+
+    // read the file
+    std::streampos begin, end;
+    std::ifstream myfile(filename.c_str(), std::ios::binary);
+    begin = myfile.tellg();
+    myfile.seekg(0, std::ios::end);
+    end = myfile.tellg();
+    std::streampos size = end - begin;
+
+    myfile.seekg(0, std::ios::beg);
+    char *memblock = new char[size];
+    myfile.read(memblock,
+                size); // read file; it's saved in the char array memblock
+    myfile.close();
+
+    std::string copyOFfile = filename + ".Copy";
+    std::ofstream myfile2(copyOFfile.c_str(), std::ios::binary);
+    myfile2.write(memblock, size); // write to a file
+    myfile2.close();
+
+    if (argv[1][0] == 's') {
+      std::cout << "Signing document.\n";
+
+      // sha256 hash of file stored as base 16 BigUnsigned
+      BigUnsigned hash_16 = BigUnsignedInABase(sha256(memblock), 16);
+      std::cout << "hash is " << hash_16 << '\n';
+
+      // get d_n.txt file, store to d and n
+      std::ifstream d_n_file("d_n.txt");
+      std::string line;
+      BigUnsigned d, n;
+      std::getline(d_n_file, line);
+      d = stringToBigUnsigned(line);
+      std::getline(d_n_file, line);
+      n = stringToBigUnsigned(line);
+      d_n_file.close();
+
+      // decryption to sign, then save to signature file
+      BigUnsigned signature = modexp(hash_16, d, n);
+      save_signature_file(signature, filename);
+    } else {
+
+      std::string sig_file_name = argv[3];
+      std::cout << "Verifying document.\n";
+      // sha256 hash of file stored as base 16 BigUnsigned
+      BigUnsigned hash_16 = BigUnsignedInABase(sha256(memblock), 16);
+      std::cout << "hash is " << hash_16 << '\n';
+
+      // get e_n.txt file, store to e and n
+      std::ifstream e_n_file("e_n.txt");
+      std::string line;
+      BigUnsigned e, n;
+      std::getline(e_n_file, line);
+      e = stringToBigUnsigned(line);
+      std::getline(e_n_file, line);
+      n = stringToBigUnsigned(line);
+      e_n_file.close();
+
+      // get signature file, store it as base 16 BigUnsigned
+      std::ifstream sig_file(sig_file_name);
+      std::getline(sig_file, line);
+      BigUnsigned signature = BigUnsignedInABase(stringToBigUnsigned(line), 16);
+
+      // encryption to verify
+      BigUnsigned verification = modexp(signature, e, n);
+
+      if (verification == hash_16) {
+        std::cout << "authentic\n";
+      } else {
+        std::cout << "modified\n";
+      }
+    }
+    delete[] memblock;
+  }
+  return 0;
+}
